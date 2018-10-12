6d435568b12ea4c8941a8a138b835d5705bacd97
calvinpete <tinkacalvin@gmail.com>
2018-10-12 13:24:39 +0300

    [Delivers #161141159] build login.html behaviour

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+function show(event, loginContent) {
+    // Hide all elements within class="container" by default */
+    var i, container, login_link;
+    container = document.getElementsByClassName("personal_details");
+    for (i = 0; i < container.length; i++) {
+        container[i].style.display = "none";
+    }
+    // Get all elements with class="loglink" and remove the class "active"
+    login_link = document.getElementsByClassName("login_link");
+    for (i = 0; i < login_link.length; i++) {
+        login_link[i].className = login_link[i].className.replace(" active", "");
+    }
+    // Show the current login form, and add an "active" class to the button that opened the login form
+    document.getElementById(loginContent).style.display = "flex";
+    event.currentTarget.className += " active";
+}
